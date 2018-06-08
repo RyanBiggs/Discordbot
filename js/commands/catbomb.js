2ccac2345b1318af05943789b5d16a6001486ac2
Ryan Biggs <ryan.biggs@outlook.com>
2018-06-08 13:13:59 +0100

    Fixed catbomb random
    Fixed the issue of giving the same gif everytime

--- js/commands/catbomb.js
+++ js/commands/catbomb.js
@@ -8,14 +8,18 @@ module.exports = {
     name: 'catbomb',
     description: 'Cats!',
     execute(message, args) {
-    	var cats = ["https://media.giphy.com/media/10GxM72EtisfFS/giphy.gif", "https://media.giphy.com/media/26FPCXdkvDbKBbgOI/giphy.gif", "https://media.giphy.com/media/l41lVf2ChyKFL513q/giphy.gif", "https://media.giphy.com/media/efHwZH4DeN9ss/giphy.gif", "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif"];
-    	var randomCat = cats[Math.floor(Math.random() * cats.length)];
-    	if (args <= 25) {
-    	var count;
-    	for(count = 0; count < args; count++){
+        var cats = ["https://media.giphy.com/media/10GxM72EtisfFS/giphy.gif", "https://media.giphy.com/media/26FPCXdkvDbKBbgOI/giphy.gif", "https://media.giphy.com/media/l41lVf2ChyKFL513q/giphy.gif", "https://media.giphy.com/media/efHwZH4DeN9ss/giphy.gif", "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif"];
+        var len = cats.length;
+        console.log(len);
+        if (args <= 25) {
+        var count;
+        for(count = 0; count < args; count++){
+            var rand = Math.floor(Math.random() * len);
+            var randomCat = cats[rand];
+            console.log(rand);
         message.channel.send(randomCat);
    } } else {
-    	message.reply('Thats an absurd amount, make it a little lower please')
+        message.reply('Thats an absurd amount, make it a little lower please')
     }
-	}
+    }
     };
